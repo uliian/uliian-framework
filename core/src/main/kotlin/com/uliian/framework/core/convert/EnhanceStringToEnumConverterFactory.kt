2a5d4c69f469d13f29eff45c7902d598926ebfb0
uliian <yncxcxz@126.com>
2021-03-17 21:05:26 +0800

    新增查询字符串enum 注解支持

--- core/src/main/kotlin/com/uliian/framework/core/convert/EnhanceStringToEnumConverterFactory.kt
+++ core/src/main/kotlin/com/uliian/framework/core/convert/EnhanceStringToEnumConverterFactory.kt
@@ -1,5 +1,6 @@
 package com.uliian.framework.core.convert
 
+import com.baomidou.mybatisplus.annotation.EnumValue
 import org.springframework.core.convert.converter.Converter
 import org.springframework.core.convert.converter.ConverterFactory
 import org.springframework.util.Assert
@@ -22,9 +23,22 @@ class EnhanceStringToEnumConverterFactory: ConverterFactory<String, Enum<*>> {
     private inner class StringToEnumConverter<E : Enum<*>>(private val enumType: Class<E>) : Converter<String, E> {
 
         fun getEnumValue(enumClassName: String, enumValue: String): Any {
-            val enumClz = Class.forName(enumClassName).enumConstants
+            val enumClz = Class.forName(enumClassName).enumConstants.map {
+                it as Enum<*>
+            }
+
+
             return enumClz.first {
                 val enum = it as Enum<*>
+                val field = it.javaClass.declaredFields.firstOrNull { it.getDeclaredAnnotation(EnumValue::class.java)!=null }
+                if (field != null) {
+                    val filedName = field.name
+                    val getMethodName = filedName[0].toUpperCase() + filedName.substring(1)
+                    val getMethod = enumType.getMethod("get${getMethodName}")
+                    return enumClz.firstOrNull{
+                        getMethod.invoke(it).toString() == enumValue.toString()
+                    } as Any
+                }
                 if(enum.name.equals(enumValue, ignoreCase = true)){
                     true
                 }else {
