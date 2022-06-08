ef930242337440151a8adc85b30d0fa215e9643b
uliian <yncxcxz@126.com>
2022-06-08 09:13:57 +0800

    升级 spring boot 到2.7.0 因为 spring fox 从2020年10月后不再更新，移除spring fox，替换为springdoc-openapi-ui

--- core/src/main/kotlin/com/uliian/framework/core/config/SwaggerConfig.kt
+++ core/src/main/kotlin/com/uliian/framework/core/config/SwaggerConfig.kt
@@ -1,77 +1,37 @@
 package com.uliian.framework.core.config
 
+
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.ExternalDocumentation
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import springfox.documentation.builders.PathSelectors
-import springfox.documentation.builders.RequestHandlerSelectors
-import springfox.documentation.oas.annotations.EnableOpenApi
-import springfox.documentation.service.ApiKey
-import springfox.documentation.service.AuthorizationScope
-import springfox.documentation.service.SecurityReference
-import springfox.documentation.service.SecurityScheme
-import springfox.documentation.spi.DocumentationType
-import springfox.documentation.spi.service.contexts.SecurityContext
-import springfox.documentation.spring.web.plugins.Docket
-import springfox.documentation.swagger.web.*
-import springfox.documentation.swagger2.annotations.EnableSwagger2
 import java.util.*
 
-@EnableOpenApi
+
+//@EnableOpenApi
 @Configuration
 class SwaggerConfig {
-    @Bean
-    fun productApi(): Docket {
-        return Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build()
-                .securitySchemes(Arrays.asList(apiKey()))
-                .securityContexts(Arrays.asList(securityContext()))
-    }
+    @Value("\${spring.application.name}")
+    private val applicationName: String? = null
 
     @Bean
-    fun uiConfig(): UiConfiguration {
-        return UiConfigurationBuilder.builder()
-                .deepLinking(true)
-                .displayOperationId(false)
-                .defaultModelsExpandDepth(1)
-                .defaultModelExpandDepth(1)
-                .defaultModelRendering(ModelRendering.EXAMPLE)
-                .displayRequestDuration(false)
-                .docExpansion(DocExpansion.NONE)
-                .filter(false)
-                .maxDisplayedTags(null)
-                .operationsSorter(OperationsSorter.ALPHA)
-                .showExtensions(false)
-                .tagsSorter(TagsSorter.ALPHA)
-                .supportedSubmitMethods(UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS)
-                .validatorUrl(null)
-                .build()
-    }
-
-    @Bean
-    fun security(): SecurityConfiguration {
-        return SecurityConfigurationBuilder.builder().scopeSeparator(",")
-                .additionalQueryStringParams(null)
-                .useBasicAuthenticationWithAccessCodeGrant(false).build()
-    }
-
-    private fun apiKey(): SecurityScheme {
-        return ApiKey("apiKey", "Authorization", "header")
-    }
-
-    private fun securityContext(): SecurityContext {
-        return SecurityContext.builder().securityReferences(defaultAuth())
-                .forPaths(PathSelectors.any()).build()
-    }
-
-    private fun defaultAuth(): List<SecurityReference> {
-        val authorizationScope = AuthorizationScope(
-                "global", "accessEverything")
-        val authorizationScopes = arrayOfNulls<AuthorizationScope>(1)
-        authorizationScopes[0] = authorizationScope
-        return Arrays.asList(SecurityReference("apiKey",
-                authorizationScopes))
+    open fun springShopOpenAPI(): OpenAPI? {
+        return OpenAPI()
+            .components(
+                Components().addSecuritySchemes(
+                    "bearer-jwt",
+                    SecurityScheme().type(SecurityScheme.Type.APIKEY)
+                        .`in`(SecurityScheme.In.HEADER).name("Authorization")
+                ))
+            .info(
+                Info().title("$applicationName Api Doc")
+            ) .addSecurityItem(
+                SecurityRequirement().addList("bearer-jwt", Arrays.asList("read", "write"))
+            )
     }
 }
--- demo/src/main/java/com/uliian/framework/demo/controller/TestController.kt
+++ demo/src/main/java/com/uliian/framework/demo/controller/TestController.kt
@@ -2,6 +2,7 @@ package com.uliian.framework.demo.controller
 
 import com.uliian.framework.demo.enums.TestE
 import com.uliian.idGenerate.EasyGenerator
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/test")
 class TestController(private val idg:EasyGenerator){
+//    @SecurityRequirement(name="auth")
     @GetMapping("")
     fun teste1(): Long {
         return idg.newId()
