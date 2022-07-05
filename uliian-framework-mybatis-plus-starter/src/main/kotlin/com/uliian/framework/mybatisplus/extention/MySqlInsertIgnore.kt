9309b1c9e41b2962fcbc028d1421397a5a9e6958
uliian <yncxcxz@126.com>
2022-07-05 18:57:35 +0800

    cgs

--- uliian-framework-mybatis-plus-starter/src/main/kotlin/com/uliian/framework/mybatisplus/extention/MySqlInsertIgnore.kt
+++ uliian-framework-mybatis-plus-starter/src/main/kotlin/com/uliian/framework/mybatisplus/extention/MySqlInsertIgnore.kt
@@ -14,34 +14,6 @@ import org.apache.ibatis.mapping.MappedStatement
 
 
 class MySqlInsertIgnore : AbstractMethod() {
-//    override fun injectMappedStatement(
-//        mapperClass: Class<*>,
-//        modelClass: Class<*>,
-//        tableInfo: TableInfo
-//    ): MappedStatement {
-//        val sql = "INSERT IGNORE INTO %s %s values %s"
-//        val fieldSql = StringBuilder()
-//        fieldSql.append(tableInfo.keyColumn).append(",")
-//        val valueSql = StringBuilder()
-//        valueSql.append("#{").append(tableInfo.keyProperty).append("},")
-//        tableInfo.fieldList.forEach { x ->
-//            fieldSql.append(x.column).append(",")
-//            valueSql.append("#{").append(x.property).append("},")
-//        }
-//        fieldSql.delete(fieldSql.length - 1, fieldSql.length)
-//        fieldSql.insert(0, "(")
-//        fieldSql.append(")")
-//        valueSql.insert(0, "(")
-//        valueSql.delete(valueSql.length - 1, valueSql.length)
-//        valueSql.append(")")
-//        val sqlSource = languageDriver.createSqlSource(
-//            configuration,
-//            String.format(sql, tableInfo.tableName, fieldSql.toString(), valueSql.toString()),
-//            modelClass
-//        )
-//        return addInsertMappedStatement(mapperClass, modelClass, "insertIgnore", sqlSource, NoKeyGenerator(), null, null)
-//    }
-
     override fun injectMappedStatement(
         mapperClass: Class<*>?,
         modelClass: Class<*>?,
@@ -54,23 +26,23 @@ class MySqlInsertIgnore : AbstractMethod() {
             LEFT_BRACKET, RIGHT_BRACKET, null, COMMA
         )
         val valuesScript = SqlScriptUtils.convertTrim(
-            tableInfo!!.getAllInsertSqlPropertyMaybeIf(null),
+            tableInfo.getAllInsertSqlPropertyMaybeIf(null),
             LEFT_BRACKET, RIGHT_BRACKET, null, COMMA
         )
-        var keyProperty: kotlin.String? = null
-        var keyColumn: kotlin.String? = null
+        var keyProperty: String? = null
+        var keyColumn: String? = null
         // 表包含主键处理逻辑,如果不包含主键当普通字段处理
         // 表包含主键处理逻辑,如果不包含主键当普通字段处理
-        if (StringUtils.isNotBlank(tableInfo!!.keyProperty)) {
-            if (tableInfo!!.idType == IdType.AUTO) {
+        if (StringUtils.isNotBlank(tableInfo.keyProperty)) {
+            if (tableInfo.idType == IdType.AUTO) {
                 /** 自增主键  */
                 keyGenerator = Jdbc3KeyGenerator()
-                keyProperty = tableInfo!!.keyProperty
-                keyColumn = tableInfo!!.keyColumn
+                keyProperty = tableInfo.keyProperty
+                keyColumn = tableInfo.keyColumn
             } else {
-                if (null != tableInfo!!.keySequence) {
+                if (null != tableInfo.keySequence) {
                     keyGenerator = TableInfoHelper.genKeyGenerator(getMethod(sqlMethod), tableInfo, builderAssistant)
-                    keyProperty = tableInfo!!.keyProperty
+                    keyProperty = tableInfo.keyProperty
                     keyColumn = tableInfo.keyColumn
                 }
             }
