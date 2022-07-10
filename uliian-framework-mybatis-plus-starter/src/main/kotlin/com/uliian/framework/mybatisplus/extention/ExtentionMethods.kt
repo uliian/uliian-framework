74811c01fad4bda21d222a677a80e17300d1fe4e
uliian <yncxcxz@126.com>
2022-07-10 21:27:47 +0800

    bug fix:changes

--- uliian-framework-mybatis-plus-starter/src/main/kotlin/com/uliian/framework/mybatisplus/extention/ExtentionMethods.kt
+++ uliian-framework-mybatis-plus-starter/src/main/kotlin/com/uliian/framework/mybatisplus/extention/ExtentionMethods.kt
@@ -6,18 +6,16 @@ import com.baomidou.mybatisplus.extension.service.IService
 import com.uliian.framework.mybatisplus.AntdPage
 import com.uliian.framework.mybatisplus.OffsetPageResult
 import kotlin.reflect.KMutableProperty1
-import kotlin.reflect.KProperty
 
 fun <T : Any,K : Comparable<K>> IService<T>.offsetPage(condition: KtQueryWrapper<T>, keySelect: KMutableProperty1<T, K?>, offset: K, size: Int): OffsetPageResult<T,K> {
     val records = this.list(condition.gt(keySelect, offset).last("limit ${size + 1}"))
     return if (records.size > size) {
         val resultRecords = records.subList(0, size)
-//        val newOffset = computeMaxOffset(resultRecords, keySelect)
         val newOffset = resultRecords.maxByOrNull { keySelect.call(it)!! }
-        OffsetPageResult(resultRecords, true,keySelect.call(newOffset))
+        OffsetPageResult(resultRecords, true,if(newOffset != null)keySelect.call(newOffset) else null)
     } else {
         val newOffset = records.maxByOrNull { keySelect.call(it)!! }
-        OffsetPageResult(records, false,keySelect.call(newOffset))
+        OffsetPageResult(records, false,if(newOffset != null)keySelect.call(newOffset) else null)
     }
 }
 
